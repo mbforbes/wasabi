137d626bb07d2eba7eb50a22d5da0742b2d5e78f
Maxwell Forbes <m.b.forbes@gmail.com>
2013-09-08 17:17:44 -0700

    Start editing level editor and start trying to figure out coordinate system, camera, and textures.

--- wasabi/src/com/mortrag/ut/wasabi/LevelEditor.java
+++ wasabi/src/com/mortrag/ut/wasabi/LevelEditor.java
@@ -1,13 +1,91 @@
 package com.mortrag.ut.wasabi;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class LevelEditor implements Screen {
 
+	// --------------------------------------------------------------------------------------------
+	// CONSTANTS
+	// --------------------------------------------------------------------------------------------
+	private static final int GRID_SPACING = 10;
+	
+	
+	// --------------------------------------------------------------------------------------------
+	// MEMBERS
+	// --------------------------------------------------------------------------------------------
+	
+	//private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture texture;
+	private Sprite sprite;	
+	private ShapeRenderer shapeRenderer;
+	
+	float w;
+	float h;
+	
+	public LevelEditor(Game g) {
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
+		
+		//camera = new OrthographicCamera(1, h/w);
+		batch = new SpriteBatch();
+		
+
+		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
+		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+		
+		sprite = new Sprite(region);
+		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+		sprite.setPosition(0, 0);
+		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		
+		shapeRenderer = new ShapeRenderer();
+	}
+	
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		//batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		sprite.draw(batch);
+		batch.end();	
+		
+		drawGrid(GRID_SPACING);
+		
+	}
+	
+	private void drawGrid(int spacing) {
+		// set up renderer
+		//shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.begin(ShapeType.Line);
+		shapeRenderer.setColor(0.5f, 0.5f, 0.5f, 0.0f); // alpha doesn't do anything...
+
+		// vertical lines
+		shapeRenderer.line(0, 100, 200, 300);
+//		for (int i = 0; i <= w; i += spacing) {
+//			shapeRenderer.line(i, top, i * 1.0f, bottom);
+//		}
+
+		// horizontal lines
+//		for (int i = bottom; i <= top; i += (int)(inc *= spacing)) {
+//			shapeRenderer.line(left, i, right, i);
+//		}
+		shapeRenderer.end();		
 	}
 
 	@Override
@@ -42,8 +120,8 @@ public class LevelEditor implements Screen {
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		batch.dispose();
+		texture.dispose();
 	}
 
 }
--- wasabi/src/com/mortrag/ut/wasabi/WasabiGame.java
+++ wasabi/src/com/mortrag/ut/wasabi/WasabiGame.java
@@ -1,7 +1,9 @@
 package com.mortrag.ut.wasabi;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -10,57 +12,12 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class WasabiGame implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	
-	@Override
-	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
-	}
+public class WasabiGame extends Game implements ApplicationListener {
 
 	@Override
-	public void dispose() {
-		batch.dispose();
-		texture.dispose();
+	public void create() {
+		Screen levelEditor = new LevelEditor(this);
+		setScreen(levelEditor);
 	}
 
-	@Override
-	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-	}
-
-	@Override
-	public void pause() {
-	}
-
-	@Override
-	public void resume() {
-	}
 }
